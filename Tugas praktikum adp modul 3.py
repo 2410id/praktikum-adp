a90752d8de0e9c54211a5d994c2b0c35c6a8ca99
2410id <muflidiash@gmail.com>
2025-03-19 11:47:05 +0700

    Add files via upload

---
+++ Tugas praktikum adp modul 3.py
@@ -0,0 +1,16 @@
+# tugas praktikum adp modul 3 
+M = (input('masukkan modal awal investasi: '))
+S = (input('masukkan suku bunga pertahun:  '))
+T= (input('ketik target uang investasi yang ingin dicapai: '))
+U = ('jumlah uang')
+
+while U<T:
+    U = (M+(M*(S/100)))
+    print (' jumlah uang pada tahun ke {i}')
+    
+for i in range () 
+    print ('tampilkan jumlah uang pada tahun ke ',i+1)
+    print ('tampilkan jumlah uang pada' )
+print (f'tampilkan uang pada tahun ke {i}')
+
+
