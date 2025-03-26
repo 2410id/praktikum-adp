85c3abd56b85dd6d9dc2311e73c52f4c8515a797
2410id <muflidiash@gmail.com>
2025-03-26 12:13:17 +0700

    Add files via upload

--- Tugas modul 4 soal pertama.py
+++ Tugas modul 4 soal pertama.py
@@ -1,1 +1,14 @@
-input
+# soal pertama
+
+baris = int(input("Masukkan jumlah baris: "))
+kolom = int(input("Masukkan jumlah kolom: "))
+
+# Perulangan bersarang untuk mencetak pola
+for i in range(baris):
+    for j in range(kolom):
+        if (i + j) % 2 == 0:
+            print("X", end=" ")
+        else:
+            print("O", end=" ")
+    print()  
+
